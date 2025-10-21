575ca38350a2e62d31838960552c463f8b667c7c
Tomás Ignacio Corbalán <tomasignaciocorbalan@gmail.com>
2025-10-21 19:23:45 -0300

    fix: se agrega obtencion de API_SERVER mediante process.env

--- frontend/src/config/constant.js
+++ frontend/src/config/constant.js
@@ -1,7 +1,7 @@
 export const BASENAME = ''; // don't add '/' at end off BASENAME
 export const BASE_URL = '/app/dashboard/default';
 export const BASE_TITLE = ' | Diplo DevOps Example App ';
-export const API_SERVER = 'http://localhost:8000/api/';
+export const API_SERVER = process.env.REACT_APP_API_SERVER || 'http://localhost:8000/api/';
 
 export const CONFIG = {
     layout: 'vertical', // disable on free version
