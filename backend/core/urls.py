53e224d94bb769bf7fba560b66512caf35f75a39
Tomás Ignacio Corbalán <tomasignaciocorbalan@gmail.com>
2025-10-27 22:28:09 -0300

    feat(docker-compose): Agregado de ruta para testear salud de los servicios

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
+from api.health import health
 
 urlpatterns = [
     path("api/users/", include(("api.routers", "api"), namespace="api")),
+    path('health/', health)
 ]
